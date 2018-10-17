3cfe5fcf9ff559a5b67813ee3941627bd74d16ec
YoungJung <dudwns9073@nate.com>
2018-10-17 20:58:10 +0900

    greedy_basic

---
+++ 그래프의 최소비용/greedy.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+using namespace std; 
+
+int main(void) {
+	int n, result=0;
+	cin >> n;
+	result += n / 500;
+	n %= 500;
+	result += n / 100;
+	n %= 100;
+	result += n / 50; 
+	n %= 50;
+	result += n / 10;
+	cout << result;
+	return 0;
+}
