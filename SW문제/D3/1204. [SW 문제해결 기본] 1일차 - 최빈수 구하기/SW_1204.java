c266f611b49f2f01ab4af31ffd9d2d565fa69130
YoungJung <dudwns9073@nate.com>
2018-11-05 16:38:10 +0900

    SW_1204

---
+++ SW문제/D3/1204. [SW 문제해결 기본] 1일차 - 최빈수 구하기/SW_1204.java
@@ -0,0 +1,37 @@
+package study_day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SW_1204 {
+public static void main(String[] args) throws IOException {
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	int T = Integer.parseInt(bf.readLine());
+	int total_array[];
+	int max = 0;
+	int index = -1;
+	for(int loop=1; loop <= T; loop++) {
+		index = -1;
+		max = 0;
+		bf.readLine();
+		String array[] = bf.readLine().split(" ");
+		
+		total_array = new int [array.length];
+		Arrays.fill(total_array,0);
+		
+		for(int i =0; i < array.length; i++) 
+			total_array[Integer.parseInt(array[i])]++;
+		for(int i = 0; i <array.length; i++) {
+			if(max <= total_array[i] && index < i) {
+				max = total_array[i];
+				index = i;
+			}
+		}
+		System.out.println("#"+loop+" "+index);
+	}
+	
+}
+}
