f8a2bf2130726dd4a2cbb9dc2a1988a1d2a4aa74
YoungJung <dudwns9073@nate.com>
2018-11-16 21:15:35 +0900

    자신의 위칫값 차의 합

---
+++ ARRAY/자신의 위칫값 차의 합.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+bool isWall(int x, int y){
+	if( x < 0 || x >= 5) return true;
+	if( y < 0 || y >= 5) return true;
+	return false;
+}
+
+int calAbs(int a1, int a2) {
+	return (a1 - a2) > 0 ? (a1 - a2) : -(a1 - a2);
+}
+
+void main() {
+	int ary[][5] = {
+		{9 , 20 , 2, 18 ,11},
+		{19, 1, 25, 3, 21},
+		{8, 24, 10, 17, 7},
+		{15, 4, 16, 5, 6},
+		{12, 13, 22, 23, 14}
+	};
+	
+	int dx[] = { 0, 0, -1, 1}; // »óÇÏ ÁÂ¿ì 
+	int dy[] = {-1, 1, 0, 0};
+	int newX, newY;
+	int sum = 0;
+	
+	for(int y =0; y <5; y++){
+		for(int x =0; x < 5; x++) {
+			for(int dir = 0; dir < 4; dir++) {
+				newX = x + dx[dir];
+				newY = y + dy[dir];
+				if(!isWal(newX, newY)) 
+					sum += calAbs(ary[y][x], ary[newY][newX]);
+			}
+		}
+	}
+	
+	printf("sum = %d\n", sum);
+}
